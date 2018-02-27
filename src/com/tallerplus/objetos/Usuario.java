3a589809e7b49e6e281dd19559099031337be94c
danimartinezsaa <danimartinezsaa@users.noreply.github.com>
2018-02-27 17:38:52 +0100

    Añadidos atributos, constructor, getters and setters

--- src/com/tallerplus/objetos/Usuario.java
+++ src/com/tallerplus/objetos/Usuario.java
@@ -1,4 +1,43 @@
 package com.tallerplus.objetos;
 
 public class Usuario {
+    //tipo:admin,recepcion,mecanico.
+    private String usuario,contrasena,tipo;
+
+    public Usuario(String usuario, String contraseña, String tipo) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contraseña) {
+        this.contrasena = contraseña;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario:" + usuario + ", contraseña:" + contrasena + ", tipo:" + tipo;
+    }
+    
+    
 }
