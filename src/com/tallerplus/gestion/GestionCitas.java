48607442d7d3997f4f3aa8a5e8449177c9bf3a3e
jose <jose@jose-PC>
2018-02-27 21:50:01 +0100

    gestion

--- src/com/tallerplus/gestion/GestionCitas.java
+++ src/com/tallerplus/gestion/GestionCitas.java
@@ -1,4 +1,107 @@
 package com.tallerplus.gestion;
 
+import com.tallerplus.objetos.Cita;
+import java.util.ArrayList;
+
 public class GestionCitas {
+    /**
+     * añade una cita para un vehiculo en la lista de citas que le pasamos 
+     * @param matricula matricula del vehiculo de la cita
+     * @param fechaHora fecha y hora de la cita en cuestion
+     * @param descripcion descripcion del problema del vehiculo
+     * @param precio precio de la reparacion
+     * @param estado estado de la reparacion
+     * @param citas lista de citas de los vehiculos 
+     * @return citas lista de citas de todos los vehiculos con la nueva cita añadida
+     */
+    public static ArrayList<Cita>anadirCita(String matricula,String fechaHora,String descripcion,float precio,String estado,ArrayList<Cita>citas){
+        citas.add(new Cita(matricula,fechaHora,descripcion,precio,estado));
+        return citas;
+    }
+    /**
+     * borra las citas de la lista de citas que tenemos 
+     * @param matricula matricula del vehiculo que tiene la cita a eliminar
+     * @param fechaHora momento de la cita
+     * @param citas lista de citas 
+     * @return citas con la cita en cuestion eliminada, siempre que fuera encontrada 
+     */
+    public static ArrayList<Cita>borrarCita(String matricula,String fechaHora,ArrayList<Cita>citas){
+        boolean borrado=false; // indicador de borrado que solo cambia a true si se encuentra la cita en cuestion
+        for(int i=0;i<citas.size();i++){
+            if(citas.get(i).getMatricula()==matricula || citas.get(i).getFechaHora()==fechaHora)
+                citas.remove(i);
+                borrado=true;
+                break;
+        }
+        // implementar en graficos
+        if(borrado==true)
+            System.out.println("cita eliminada");
+        else System.out.println("cita no encontrada");
+        return citas;
+        
+    }
+    /**
+     * modifica las citas ya creadas dentro de una lista de citas 
+     * @param matricula matricula de la cita a modificar , este dato no puede ser modificado
+     * @param fechaHora fecha y hora de la cita a modificar , este dato no puede ser modificado
+     * @param descripcion descripcion de la cita, modificada o no 
+     * @param precio precio de la reparacion, introducida o no
+     * @param estado estado de la reparacion, modificada o no
+     * @param citas lista de citas 
+     * @return citas con la cita ya modificada
+     */
+    public static ArrayList<Cita>modificarCita(String matricula,String fechaHora,String descripcion,float precio,String estado,ArrayList<Cita>citas){ // no se pueden modificar los campo clave matricula y fecha, en caso de que esos datos esten mal hay que borrar la cita y generar una nueva
+        boolean modificado=false;
+        for(int i=0;i<citas.size();i++){
+            if(citas.get(i).getMatricula()==matricula || citas.get(i).getFechaHora()==fechaHora)
+                if (citas.get(i).getDescripcion()!=descripcion){
+                    citas.get(i).setDescripcion(descripcion);
+                    modificado=true;
+                }
+            if(citas.get(i).getPrecio()!=precio){
+                citas.get(i).setPrecio(precio);
+                modificado=true;
+            }
+            if(citas.get(i).getDescripcion()!=descripcion){
+                citas.get(i).setDescripcion(descripcion);
+                modificado=true;
+            }
+            break;
+        }
+        // implementar
+        if (modificado==true)
+            System.out.println("cita modificada");
+        else System.out.println("cita no encontrada, no se ha podido modificar");
+        return citas;
+    }
+    /**
+     * consulta la cota de una determinada hora de un determinado dia
+     * @param fechaHora dato de fecha y hora para realizar la consulta
+     * @param citas lista de citas
+     * @return citaConsultada la cita concreta que buscamos
+     */
+    public static Cita consultarCita(String fechaHora,ArrayList<Cita>citas){ // podemos consultar una cita segun su hora, si no se sabe la hora de una cita de una determinada matricula se debe usar el historial del vehiculo
+        Cita citaConsultada=new Cita();
+        for(int i=0;i<citas.size();i++){
+            if(citas.get(i).getFechaHora().equals(fechaHora))
+                citaConsultada=citas.get(i);
+        }
+        return citaConsultada;
+    }
+    /**
+     * ver el historial de citas de un determinado vehiculo
+     * @param matricula matricula del vehiculo que queremos sacar su historial
+     * @param citas lista con el historial de citas 
+     * @return citaCoche lista de todas las citas de ese determinado vehiculo
+     */
+    public static ArrayList<Cita> verHistorial(String matricula,ArrayList<Cita>citas){ // ver historial de un determinado vehiculo
+        ArrayList<Cita>citaCoche=new ArrayList<>();
+        for(int i=0;i<citas.size();i++){
+            if(citas.get(i).getMatricula()==matricula){
+                citaCoche.add(citas.get(i));
+            }
+            
+        }
+        return citaCoche;
+    }
 }
--- src/com/tallerplus/gestion/GestionClientes.java
+++ src/com/tallerplus/gestion/GestionClientes.java
@@ -1,4 +1,48 @@
 package com.tallerplus.gestion;
 
+import com.tallerplus.objetos.Coche;
+import java.util.ArrayList;
+
 public class GestionClientes {
+    /**
+     * Añade un cliente al ArrayList de clientes que recibe y lo devuelve
+     * @param matricula matricula del vehiculo a reparar
+     * @param motor tipo de combustible, diesel o gasolina
+     * @param cilindrada cilindrada del vehiculo en cuestion
+     * @param caballos potencia del vehiculo expresada en caballos de vapor
+     * @param nombreDueño nombre del propietario del vehiculo
+     * @param dni numero de identificacion personal del dueño del vehiculo
+     * @param telefono telefono de contacto del dueño del vehiculo
+     * @param coches lista de coches que son reparados en el taller
+     * @return coches lista de coches que son reparados en el taller, una vez añadimos el nuevo vehiculo
+     */
+    public static ArrayList<Coche> anadirCliente(String matricula,String motor,String cilindrada,String caballos,String nombreDueño,String dni,String telefono,ArrayList<Coche>coches){
+    coches.add(new Coche(matricula,motor,cilindrada,caballos,nombreDueño,dni,telefono));
+    return coches;
+    }
+    /**
+     * Elimina un vehiculo de la lista de vehiculos que tenemos en el taller
+     * @param matricula matricula del vehiculo a eliminar
+     * @param coches lista de coches con los que trabajamos en el taller
+     * @return coches lista de coches que trabajamos en el taller, con el vehiculo en cuestion ya eliminado
+     */
+    public static ArrayList<Coche> borrarCliente(String matricula,ArrayList<Coche>coches){
+        boolean borrado=false; // indicador de borrado que solo salta a true en caso de que se encuentre el coche, posteriormente trataremos que no se encuentre con el 
+        for(int i=0;i<coches.size();i++){
+            if(coches.get(i).getMatricula().equals(matricula)){
+                coches.remove(i);
+                borrado=true;
+                break;
+            }
+        
+        }
+        // implementar en graficos
+        if(borrado==true)
+            System.out.println("vehiculo borrado");
+        else System.out.println("no se ha encontrado el vehiculo a eliminar");
+        return coches;
+    }
+    
 }
+
+
