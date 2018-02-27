7c1c24b96503fc4f3170317962828df9c36f8d7a
danimartinezsaa <danimartinezsaa@users.noreply.github.com>
2018-02-27 18:11:09 +0100

    métodos añadir y borrar usuarios implementados.

--- src/com/tallerplus/gestion/GestionUsuarios.java
+++ src/com/tallerplus/gestion/GestionUsuarios.java
@@ -1,4 +1,23 @@
 package com.tallerplus.gestion;
 
+import com.tallerplus.objetos.Usuario;
+import java.util.ArrayList;
+
 public class GestionUsuarios {
+    public static ArrayList<Usuario> anadirUsuario(String usuario,String contrasena, String tipo,ArrayList<Usuario> usuarios){
+        usuarios.add(new Usuario(usuario,contrasena,tipo));
+        return usuarios;
+    }
+    
+    public static ArrayList<Usuario> borrarUsuario(String usuario,ArrayList<Usuario> usuarios){
+        int posicion=0;
+        for(int i=0;i<usuarios.size();i++){
+            if(usuarios.get(i).equals(usuario)){
+                posicion=i;
+                break;
+            }
+        }
+        usuarios.remove(posicion);
+        return usuarios;
+    }
 }
