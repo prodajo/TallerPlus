42739a6aa8312f2896d58c48a00bca214d844f80
jose <jose@jose-PC>
2018-02-27 17:55:21 +0100

    objetos_de_jose

--- src/com/tallerplus/objetos/Coche.java
+++ src/com/tallerplus/objetos/Coche.java
@@ -1,4 +1,70 @@
 package com.tallerplus.objetos;
-
+// un coche sera cada uno de los objetos tratados en el taller, reparado, con datos de su dueño para poder localizarlo
 public class Coche {
+    // un coche tendra datos sobre el motor, matricula, y datos identificativos de su dueño
+    private String matricula,motor,cilindrada,caballos,nombreDueño,dni,telefono;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(String cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public String getCaballos() {
+        return caballos;
+    }
+
+    public void setCaballos(String caballos) {
+        this.caballos = caballos;
+    }
+
+    public String getNombreDueño() {
+        return nombreDueño;
+    }
+
+    public void setNombreDueño(String nombreDueño) {
+        this.nombreDueño = nombreDueño;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "matricula: "+matricula+" motor"+cilindrada+" "+motor+" "+caballos+"propietario: "+nombreDueño+" "+dni+" "+telefono;
+    }
+  
+    
+    
 }
